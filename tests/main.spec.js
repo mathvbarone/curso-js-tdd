52dba1db06c10a64baab74b169b852a16695f691
Matheus Barone <matheusvbarone@gmail.com>
2018-09-06 11:19:09 -0300

    Setting some lint configurations

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import calc from '../src/index';
 
-describe('Calc', () => {
+describe('calc', () => {
   describe('Smoke Tests', () => {
     it('should exist the calc lib', () => {
       expect(calc).to.exist;
