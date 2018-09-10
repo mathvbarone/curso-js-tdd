3e2fe2124b54ec20193ffa84b3c442bcf1a4433f
Matheus Barone <matheusvbarone@gmail.com>
2018-09-10 14:51:52 -0300

    cleaning up master branch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 // import style from './main.css';
-
-const fizzbuzz = (num) => {
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-  if (num % 3 === 0) return 'fizz';
-  if (num % 5 === 0) return 'buzz';
-
-  return num;
-};
-
-export default fizzbuzz;
